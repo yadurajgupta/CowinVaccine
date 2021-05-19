142a7cbfaba3a46c585915cb2d144dd1a01f2389
yadurajgupta <yaduraj1997@gmail.com>
2021-05-19 16:45:50 +0530

    Better refactoring of code

--- automating_select_v2.js
+++ automating_select_v2.js
@@ -1,15 +1,3 @@
-function add_jquery() {
-    let script = document.createElement("script");
-    script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min.js"
-    document.getElementsByTagName("head")[0].appendChild(script);
-};
-add_jquery()
-// Audio by Eric Matyas
-// www.soundimage.org
-let successAudio = new Audio("http://soundimage.org/wp-content/uploads/2016/04/UI_Quirky1.mp3");
-let errorAudio = new Audio("https://soundimage.org/wp-content/uploads/2020/01/UI_Quirky_42.mp3");
-let SCRIPT_URL = "https://selfregistration.cowin.gov.in/appointment"
-
 //TO BE FILLED BY USER 
 // minimum number of slots available 
 let minimum_available_slots = 1
@@ -35,11 +23,31 @@ end_date = "DD/MM/YYYY";
 
 (() => {
     let intervalVal = null;
+    let script = document.createElement("script");
+    script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min.js"
+    document.getElementsByTagName("head")[0].appendChild(script);
     setTimeout(() => {
-        CENTERS_NAMES = CENTERS_NAMES.map((name) => { return name.toLowerCase() })
+
+        // Audio by Eric Matyas
+        // www.soundimage.org
+        let successAudio = new Audio("http://soundimage.org/wp-content/uploads/2016/04/UI_Quirky1.mp3");
+        let errorAudio = new Audio("https://soundimage.org/wp-content/uploads/2020/01/UI_Quirky_42.mp3");
+        //URL on which script is meant to work on
+        let SCRIPT_URL = "https://selfregistration.cowin.gov.in/appointment"
+
+        let center_names_parsed = CENTERS_NAMES.map((name) => { return name.toLowerCase() })
+
+        // Jquery selectors for elements
+        let jquery_filters_selector = "div.agefilterblock div input"
+        let jquery_slot_selector = "ul.slot-available-wrap li a"
+        let jquery_center_name_selector = "h5.center-name-title"
+        let success_appointment_header_selector = "h3.appoint-success"
+        let search_button_selector = "ion-button.pin-search-btn"
+        let center_row_selector = "ion-row"
+        let slot_date_selector = "li.availability-date"
+
         function parse_date(date_string) {
-            if (date_string == "DD/MM/YYYY")
-                return null;
+            if (date_string == "DD/MM/YYYY") return null;
             let parsed = date_string.split("/").map((value) => { return parseInt(value) })
             return new Date(parsed[2], parsed[1] - 1, parsed[0])
         }
@@ -56,13 +64,13 @@ end_date = "DD/MM/YYYY";
             months[new Date(0, index).toLocaleString('en-US', { month: 'long' })] = index
         }
         function get_clicked_filters() {
-            return $("div.agefilterblock div input").toArray()
+            return $(jquery_filters_selector).toArray()
                 .map((element, index) => { return [element, index] })
                 .filter((element) => { return element[0].checked })
                 .map((element) => { return element[1] })
         }
         function get_dates() {
-            return $("li.availability-date").toArray()
+            return $(slot_date_selector).toArray()
                 .filter((element) => { return $(element).is(":visible") })
                 .map((element) => {
                     let arr = $(element).text().split(" ")
@@ -91,28 +99,24 @@ end_date = "DD/MM/YYYY";
                 })
         }
         function get_centers() {
-            return $("ion-row")
+            return $(center_row_selector)
                 .toArray()
                 .filter((element) => {
-                    return $(element).find("ul.slot-available-wrap li a").length > 0
-                        && $(element).find("ion-row").length == 0
+                    return $(element).find(jquery_slot_selector).length > 0
+                        && $(element).find(center_row_selector).length == 0
                 })
                 .map((element) => {
                     return {
-                        'center_name': $($(element).find("h5.center-name-title")).text().toLowerCase(),
-                        'slots': parse_slots($(element).find("ul.slot-available-wrap li a")),
+                        'center_name': $($(element).find(jquery_center_name_selector)).text().toLowerCase(),
+                        'slots': parse_slots($(element).find(jquery_slot_selector)),
                     };
                 })
                 .filter((element) => { return element['slots'].length > 0 })
         }
         function filter_by_name(center_info) {
-            if (CENTERS_NAMES.length == 0) return true;
+            if (center_names_parsed.length == 0) return true;
             let { center_name } = center_info
-            let res = CENTERS_NAMES.some((name) => { return center_name.search(name) != -1; })
-            if (res == false) {
-                console.warn([`${center_name} is being BLOCKED DUE TO CENTER NAMES RESTRICTION`, `CENTER_NAMES={${CENTERS_NAMES}}`].join("\n"))
-            }
-            return res;
+            return center_names_parsed.some((name) => { return center_name.search(name) != -1; })
         }
         function concat_and_sort_slots(accumulator, element) {
             return accumulator.concat(element['slots'])
@@ -126,10 +130,10 @@ end_date = "DD/MM/YYYY";
         let clicked_filters_indexes = get_clicked_filters()
         intervalVal = setInterval(() => {
             try {
-                if ($("h3.appoint-success").length > 0) return "DONE";
+                if ($(success_appointment_header_selector).length > 0) return "DONE";
                 if (document.location.href != SCRIPT_URL) throw ["Not at the right page", `Get to ${SCRIPT_URL}`].join("\n");
-                $("ion-button.pin-search-btn")[0].click();
-                let filters = $("div.agefilterblock div input").toArray()
+                $(search_button_selector)[0].click();
+                let filters = $(jquery_filters_selector).toArray()
                 clicked_filters_indexes.forEach(element => filters[element].click());
                 setTimeout(() => {
                     let result = get_centers()
